542281c15963605bfccf3780a066adf09bcdceab
stinkywizard23 <129058848+stinkywizard23@users.noreply.github.com>
2023-03-27 11:38:58 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,102 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "prisoner 01":return img`
+. . . . . . . f f f f f . . . . 
+. . . . . f f d d d d d f . . . 
+. . . f f d d d d d d d d f . . 
+. . . f d d f f d d f f d f . . 
+. . f d d d 1 f d d 1 f d f . . 
+. . f d d d 1 f d d 1 f d f . . 
+. . f d d d d d d d d d d f . . 
+. . . f d d d b b d d d f . . . 
+. . f 4 4 4 d d d d 4 4 4 f . . 
+. f 4 4 4 f f f 4 f f 4 4 4 f . 
+. f 4 4 4 f 4 f 4 4 f 4 4 4 f . 
+. f d d 4 f 4 f 4 4 f 4 4 d f . 
+. . f f 4 f f f 4 f f f 4 f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . . f 4 4 4 f f f 4 4 f . . . 
+. . . f f f f f . f f f f . . . 
+`;
+            case "image2":
+            case "fire drop":return img`
+. . . . . . . . . . . . . . . . . 
+. . . . . . . . f . . . . . . . . 
+. . . . . . . f 2 f . . . . . . . 
+. . . . . . . f 2 f . . . . . . . 
+. . . . . . f 4 4 2 f . . . . . . 
+. . . . . . f 2 4 2 f . . . . . . 
+. . . . . f 5 4 4 4 2 f . . . . . 
+. . . . . f 4 4 5 4 2 f . . . . . 
+. . . . f 5 4 2 4 4 4 2 f . . . . 
+. . . . f 4 4 2 2 4 4 2 f . . . . 
+. . . . f 2 4 5 4 5 2 2 f . . . . 
+. . . . . f 5 4 4 2 2 f . . . . . 
+. . . . . . f 2 4 2 f . . . . . . 
+. . . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "prisoner 0":return img`
+. . . . f f f f f . . . . . . . 
+. . . f d d d d d f f . . . . . 
+. . f d d d d d d d d f f . . . 
+. . f d f f d d f f d d f . . . 
+. . f d f 1 d d f 1 d d d f . . 
+. . f d f 1 d d f 1 d d d f . . 
+. . f d d d d d d d d d d f . . 
+. . . f d d d b b d d d f . . . 
+. . f 4 4 4 d d d d 4 4 4 f . . 
+. f 4 4 4 f f 4 f f f 4 4 4 f . 
+. f 4 4 4 f 4 4 f 4 f 4 4 4 f . 
+. f d 4 4 f 4 4 f 4 f 4 d d f . 
+. . f 4 f f f 4 f f f 4 f f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . . f 4 4 f f f 4 4 4 f . . . 
+. . . f f f f . f f f f f . . . 
+`;
+            case "image4":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . f f . . . . . . . . . . . . 
+. f 7 7 f f . . . . . . . . . . 
+. f 7 7 7 7 f f . . . . . . . . 
+. . f f 7 f 7 7 f . . . . . . . 
+. . . f 7 7 f f 7 f f f f . . . 
+. . . . f f 7 7 f 2 2 2 2 f . . 
+. . . . f f f f 2 2 2 2 2 2 f . 
+. . . f 2 2 f 2 f 2 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 f 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 f 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 f 2 2 2 f . . 
+. . f 2 2 2 2 2 2 f f f f . . . 
+. . . f 2 2 2 2 f . . . . . . . 
+. . . . f f f f . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
